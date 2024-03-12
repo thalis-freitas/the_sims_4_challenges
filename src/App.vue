f4335885b5be82625018181c2e47fb433f7539bb
Thalis Freitas <ofthalis@gmail.com>
2024-03-11 21:41:20 -0300

    correct display of challenges

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import NavBar from './components/NavBar.vue'
 import Home from './components/Home.vue'
 import Intro from './components/Intro.vue'
-import Challenges from './components/Challenges.vue';
+import Challenges from './components/Challenges.vue'
 </script>
 
 <template>
--- src/components/Challenges.vue
+++ src/components/Challenges.vue
@@ -1,8 +1,3 @@
-<script>
-import ChallengeLink from './ChallengeLink.vue'
-
-</script>
-
 <template>
   <div class="bg-secondary">
     <section
@@ -14,44 +9,80 @@ import ChallengeLink from './ChallengeLink.vue'
       </h1>
 
       <div class="flex w-full">
-        <ChallengeLink
-          link="https://desafiosptbr.tumblr.com/post/169239531401/tradu%C3%A7%C3%A3o-do-desafio-not-so-berry-pt-br"
-          imageSrc="../assets/images/challenges/not_so_berry.jpeg"
-          altText="Desafio not so berry"
-          description="Not so berry"
-        />
-        <ChallengeLink
-          link="https://thesimschallenges.wixsite.com/sims/do-lixo-ao-luxo"
-          imageSrc="../assets/images/challenges/lixo_ao_luxo.jpeg"
-          altText="Desafio do lixo ao luxo"
-          description="Lixo ao luxo"
-        />
-        <ChallengeLink
-          link="https://lalasims.wordpress.com/2016/07/27/desafio-da-viuva-negra/comment-page-1/"
-          imageSrc="../assets/images/challenges/viuva_negra.jpeg"
-          altText="Desafio da viúva negra"
-          description="Viúva negra"
-        />
+        <a
+          href="https://desafiosptbr.tumblr.com/post/169239531401/tradu%C3%A7%C3%A3o-do-desafio-not-so-berry-pt-br"
+          target="_blank"
+          class="w-1/3 p-4"
+        >
+          <img
+            src="../assets/images/challenges/not_so_berry.jpeg"
+            alt="Desafio not so berry"
+            class="w-full h-48"
+          >
+          <p class="text-center mt-2 text-nav-light-blue">Not so berry</p>
+        </a>
+        <a
+          href="https://thesimschallenges.wixsite.com/sims/do-lixo-ao-luxo"
+          target="_blank"
+          class="w-1/3 p-4"
+        >
+          <img
+            src="../assets/images/challenges/lixo_ao_luxo.jpeg"
+            alt="Desafio do lixo ao luxo"
+            class="w-full h-48"
+          >
+          <p class="text-center mt-2 text-nav-light-blue">Lixo ao luxo</p>
+        </a>
+        <a
+          href="https://lalasims.wordpress.com/2016/07/27/desafio-da-viuva-negra/comment-page-1/"
+          target="_blank"
+          class="w-1/3 p-4"
+        >
+          <img
+            src="../assets/images/challenges/viuva_negra.jpeg"
+            alt="Desafio da viúva negra"
+            class="w-full h-48"
+          >
+          <p class="text-center mt-2 text-nav-light-blue">Viúva negra</p>
+        </a>
       </div>
       <div class="flex w-full">
-        <ChallengeLink
-          link="https://thesimschallenges.wixsite.com/sims/desafio-dos-7-bebes"
-          imageSrc="../assets/images/challenges/sete_bebes.jpeg"
-          altText="Desafio dos sete bebês"
-          description="Sete bebês"
-        />
-        <ChallengeLink
-          link="https://www.ligadosgames.com/desafios-incriveis-the-sims-4/#anchor-desafio1"
-          imageSrc="../assets/images/challenges/manicomio.jpg"
-          altText="Desafio do manicômio"
-          description="Manicômio"
-        />
-        <ChallengeLink
-          link="https://www.ligadosgames.com/desafios-incriveis-the-sims-4/#anchor-desafio9"
-          imageSrc="../assets/images/challenges/vida_longa.jpg"
-          altText="Desafio vida longa"
-          description="Vida longa"
-        />
+        <a
+          href="https://thesimschallenges.wixsite.com/sims/desafio-dos-7-bebes"
+          target="_blank"
+          class="w-1/3 p-4"
+        >
+          <img
+            src="../assets/images/challenges/sete_bebes.jpeg"
+            alt="Desafio dos sete bebês"
+            class="w-full h-48"
+          >
+          <p class="text-center mt-2 text-nav-light-blue">Sete bebês</p>
+        </a>
+        <a
+          href="https://www.ligadosgames.com/desafios-incriveis-the-sims-4/#anchor-desafio1"
+          target="_blank"
+          class="w-1/3 p-4"
+        >
+          <img
+            src="../assets/images/challenges/manicomio.jpg"
+            alt="Desafio do manicômio"
+            class="w-full h-48"
+          >
+          <p class="text-center mt-2 text-nav-light-blue">Manicômio</p>
+        </a>
+        <a
+          href="https://www.ligadosgames.com/desafios-incriveis-the-sims-4/#anchor-desafio9"
+          target="_blank"
+          class="w-1/3 p-4"
+        >
+          <img
+            src="../assets/images/challenges/vida_longa.jpg"
+            alt="Desafio vida"
+            class="w-full h-48"
+          >
+          <p class="text-center mt-2 text-nav-light-blue">Viúva negra</p>
+        </a>
       </div>
     </section>
   </div>
